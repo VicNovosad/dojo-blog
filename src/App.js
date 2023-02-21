f586ae44c223e32bcae87dd3f30f087459efbbf5
Vic Novosad <vic.novosad.us@gmail.com>
2023-02-20 20:27:31 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-red-600"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reac
         </a>
+        <h1 className='text-red-400'>Hello from App.js</h1>
       </header>
     </div>
   );
